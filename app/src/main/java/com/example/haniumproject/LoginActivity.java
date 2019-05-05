79edb54869bab14530aa83eabbab30f253027d4d
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2019-05-06 00:58:29 +0900

    LoginActivity_0506
    change design

---
+++ app/src/main/java/com/example/haniumproject/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.haniumproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_login);
+        Intent intent=getIntent();
+
+    }
+
+}
